243dba3f474f02b59b933882bb0670b9b3f4e99f
arc363 <arc3363@yahoo.co.uk>
2023-10-31 17:46:59 +0000

    first step

---
+++ main.js
@@ -0,0 +1,13 @@
+// Pick randomly between shelf, chest and Curzon
+
+const randomSource = function () {
+  const randomNumber = Math.floor(Math.random() * 3);
+  const source = [`the shelf`, `the chest`, `Curzon`];
+
+  console.log(`Choose a film from...
+    ...${source[randomNumber]}!`);
+};
+
+randomSource();
+
+//
