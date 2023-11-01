79242aaa8f72dbbdb9021e6c819c1c2e3cb30c52
arc363 <arc3363@yahoo.co.uk>
2023-11-01 10:04:16 +0000

    new functions

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 // Pick randomly between shelf, chest and Curzon
 
 const randomSource = function () {
-  const randomNumber = Math.floor(Math.random() * 3);
+  const randomNumber1 = Math.floor(Math.random() * 3);
   const source = [`the shelf`, `the chest`, `Curzon`];
 
   console.log(`Choose a film from...
-    ...${source[randomNumber]}!`);
+    ...${source[randomNumber1]}!`);
 };
 
-randomSource();
+// randomSource();
 
-//
+// Take 2 - Generate array of unique numbers
+
+let arr = [];
+let numberOfFilms = 759;
+
+while (arr.length < 5) {
+  let r = Math.floor(Math.random() * numberOfFilms) + 1;
+  if (arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
+
+// Read CSV file into object
+
+// Relate array to items in array
